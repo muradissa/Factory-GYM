9084b660732fb68563d9b90ae350b0f874967bd6
Murad EIssa <muradissa00@gmail.com>
2023-05-28 01:55:57 +0300

    change too Hebrew

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
 import './hero.css';
 import Header from '../Header/Header';
-import hero_image from '../../assets/hero_image.png';
-import hero_image2 from '../../assets/hero_image2.png';
 import hero_image4 from '../../assets/hero_image4.png';
-
-
-
 import hero_image_back from '../../assets/hero_image_back.png';
-import Heart from '../../assets/heart.png';
-import Calories from '../../assets/calories.png';
 import hero_background from '../../assets/hero_background.jpg';
 import hero_image_back2 from '../../assets/bluee.png';
 import Heart2 from '../../assets/heartt.gif';
@@ -20,8 +13,10 @@ import chart from '../../assets/chart.gif';
 
 function Hero() {
   return (
-    <div className='hero' style={{background:hero_background}}>
+     <>
+     <div className='hero' style={{background:hero_background}}>
         <div className="left-h">
+            {/* Navbar */}
             <Header/>
             
             <div className="the-best-ad">
@@ -38,36 +33,32 @@ function Hero() {
               <div>
                 <span>Ideal body</span>
               </div>
-              {/* <div className='span'>
-                in here we woll help you to shape and build your ideal body and live up your life to fullest
-              </div> */}
               <div className='span'>
                 כאן נעזור לך לעצב ולבנות את הגוף האידיאלי שלך ולחיות את חייך במלואם
               </div>
             </div>
             {/* figures */}
-            <div className='figures'>
+            {/* <div className='figures'>
               <div>
-                <span>+140</span>
-                <span>Expert Coaches</span>
+                <span>+40</span>
+                <span>מכשירי כושר</span>
               </div>
               <div>
-                <span>+ 978</span>
-                <span>Members Joined</span>
+                <span>+ 200</span>
+                <span>משתתפים</span>
               </div>
               <div>
                 <span>+50</span>
-                <span>Fitness Programs</span>
+                <span>תוכניות כושר</span>
               </div>
-            </div>
+            </div> */}
             {/* hero-buttons */}
-            <div className='hero-buttons'>
+            {/* <div className='hero-buttons'>
               <button className='btn'>Get Started</button>
               <button className='btn'>Learn More</button>
-            </div>
+            </div> */}
         </div>
         <div className="right-h">
-          <button className='btn'>Join Now</button>
           
           <div className='heart-calories'>
 
@@ -92,18 +83,40 @@ function Hero() {
           <img src={hero_image4} alt="" className='hero-image'/>
           {/* <img src={hero_image_back2} alt="" className='hero-image-back'/> */}
           
-          {/* calories */}
-          {/* <div className='calories'>
-            <img src={chart} alt=""/>
-            <div>
-              <span>Calories Burned</span>
-              <span>220 kcal</span>
-            </div>
-            
-          </div> */}
+    
 
         </div>
+        {/* figures */}
+        {/* <div className='figures'>
+              <div>
+                <span>+40</span>
+                <span>מכשירי כושר</span>
+              </div>
+              <div>
+                <span>+ 200</span>
+                <span>משתתפים</span>
+              </div>
+              <div>
+                <span>+50</span>
+                <span>תוכניות כושר</span>
+              </div>
+            </div> */}
     </div>
+    <div className='figures'>
+      <div>
+        <span>40+</span>
+        <span>מכשירי כושר</span>
+      </div>
+      <div>
+        <span>200+</span>
+        <span>משתתפים</span>
+      </div>
+      <div>
+        <span>50+</span>
+        <span>תוכניות כושר</span>
+      </div>
+    </div>
+   </>
   )
 }
 
--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import './programs.css';
 import {programsData} from '../../data/programsData';
 import RightArrow from '../../assets/rightArrow.png'
+import { CgGym } from 'react-icons/cg';
+import { BiRun } from 'react-icons/bi';
+import { BsFire,BsHeartPulseFill } from 'react-icons/bs';
+
 
 function Programs() {
   return (
     <div className='programs' id="programs">
         {/* header */}
         <div className='programs-header'>
-            <span className='stroke-text'>Explore our</span>
+            {/* <span className='stroke-text'>Explore our</span>
             <span>Programs</span>
-            <span className='stroke-text'>to shape you</span>
+            <span className='stroke-text'>to shape you</span> */}
+            <span className='stroke-text'>התוכניות שלנו</span>
         </div>
 
         <div className='program-categories'>
-            {programsData.map((program)=>
+            {/* {programsData.map((program)=>
                 <div className='category'>
                     {program.image}
                     <span className=''>
@@ -30,7 +35,53 @@ function Programs() {
                         <img src={RightArrow} alt=''/>
                     </div>
                 </div>
-            )}
+            )} */}
+            <div className='category'>
+                <BsFire/>
+                <span className=''>
+                    שריפת שומנים
+                </span>
+                <span className=''>
+                    תוכנית זו מתאימה לכם שרוצים להיפטר מהשומן ולרדת במשקל
+                </span>
+                <div className='read-more'>
+                    <span>
+                    עוד
+                    </span>
+                    <img src={RightArrow} alt=''/>
+                </div>
+            </div>
+            <div className='category'>
+                <CgGym style={{color:"var(--blue)"}}/>
+                <span className=''>
+                    אימון כוח
+                    בניית שרירים 
+                </span>
+                <span className=''>
+                    בתוכנית זו, אתה מאומן לשפר את הכוח שלך באמצעות תרגילים רבים
+                </span>
+                <div className='read-more'>
+                    <span>
+                        עוד 
+                    </span>
+                    <img src={RightArrow} alt=''/>
+                </div>
+            </div>
+            <div className='category'>
+                <BsHeartPulseFill/>
+                <span className=''>
+                    בריאות וכושר
+                </span>
+                <span className=''>
+                    תוכנית זו מיועדת למי שמתאמן רק למען כושר גופם ולא לבניית הגוף
+                </span>
+                <div className='read-more'>
+                    <span>
+                    עוד     
+                    </span>
+                    <img src={RightArrow} alt=''/>
+                </div>
+            </div>
         </div>
 
     </div>
